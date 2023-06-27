71e9d1b2a997462c321354dee2a8ae8a5b1ce44b
vidadepetstore <vidadepetstore@gmail.com>
2023-06-27 16:35:43 -0300

    formulário
    Formulário

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById('enviar').addEventListener('click',function(event) {
+    event.preventDefault();
+
+    // Evita o envio padrão do formulário
+
+    // Obter os valores dos campos de entrada
+
+    let nome = document.getElementById(Nome).ariaValueMax;
+    let nome = document.getElementById(Endereço).ariaValueMax;
+    let nome = document.getElementById(Número).ariaValueMax;
+    let nome = document.getElementById(Email).ariaValueMax;
+
+    // Realizar ações com os dados
+
+    console.log('Nome:',nome)
+    console.log('Endereço',endereço)
+    console.log('Numero:',numero)
+    console.log('Email',email)
+
+
+
+
+
+
+
+
+    ) }                           
+
+
+
+
+
+
+
+
+
